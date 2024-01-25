7dff402c0a1ffcfb599ec5e494ebee6e2d190efd
musiienko25 <devmusiienko@gmail.com>
2024-01-25 16:29:29 +0200

    created auth form

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from "react";
+
+import "./App.css";
+import { Route, Routes } from "react-router-dom";
+import HomePage from "./pages/Homepage/HomePage";
+import AuthPage from "./pages/AuthPage/AuthPage";
+
+function App() {
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<HomePage />}></Route>
+        <Route path="/auth" element={<AuthPage />}></Route>
+      </Routes>
+    </>
+  );
+}
+
+export default App;
