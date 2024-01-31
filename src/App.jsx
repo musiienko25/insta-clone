4b617de44f6a9046d6a4ef1dc74ffb41d46bcec3
musiienko25 <devmusiienko@gmail.com>
2024-01-31 20:27:04 +0200

    profile page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Route, Routes } from "react-router-dom";
 import HomePage from "./pages/Homepage/HomePage";
 import AuthPage from "./pages/AuthPage/AuthPage";
 import Pagelayout from "./components/AuthFomr/Layouts/PageLayout/Pagelayout";
+import ProfilePage from "./pages/ProfilePage/ProfilePage";
 
 function App() {
   return (
@@ -12,6 +13,7 @@ function App() {
       <Routes>
         <Route path="/" element={<HomePage />}></Route>
         <Route path="/auth" element={<AuthPage />}></Route>
+        <Route path="/profile" element={<ProfilePage />}></Route>
       </Routes>
     </Pagelayout>
   );
